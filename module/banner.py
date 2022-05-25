ccfa76f110df321d159f3eb81c3d397bf608c89a
Mandofskii <59086060+Mandofskii@users.noreply.github.com>
2022-05-26 00:08:20 +0430

    Better coding ...

--- module/banner.py
+++ module/banner.py
@@ -1,24 +1,23 @@
 import os
+from subprocess import Popen
 from colorama import Fore
 import time
-from subprocess import Popen
+
+
 def banner():
     os.system("clear")
     print("")
     Popen('neofetch')
     time.sleep(2)
 
-
+def print_and_sleep(*objects, seconds=0.1):
+    time.sleep(seconds)
+    print(objects)
+   
 def show_menu():
-    time.sleep(0.1)
-    print(Fore.RED+" ["+Fore.WHITE+"*"+Fore.RED+"]"+Fore.CYAN+" Choose one of the options below \n")
-    time.sleep(0.1)
-    print(Fore.RED+" [0]"+Fore.WHITE+" Get Normal Data "+Fore.YELLOW+"[Without Any Permissions]\n")
-    time.sleep(0.1)
-    print(Fore.RED+" [1]"+Fore.WHITE+" Get Location "+Fore.GREEN+"[SMARTPHONES]\n")
-    time.sleep(0.1)
-    print(Fore.RED+" [2]"+Fore.WHITE+" Access Webcam\n") 
-    time.sleep(0.1)
-    print(Fore.RED+" [3]"+Fore.WHITE+" Access Microphone \n")
-    time.sleep(0.1)
-    print(Fore.RED+" [4]"+Fore.WHITE+" Exit . . .\n")
+    print_and_sleep(Fore.RED+" ["+Fore.WHITE+"*"+Fore.RED+"]"+Fore.CYAN+" Choose one of the options below \n")
+    print_and_sleep(Fore.RED+" [0]"+Fore.WHITE+" Get Normal Data "+Fore.YELLOW+"[Without Any Permissions]\n")
+    print_and_sleep(Fore.RED+" [1]"+Fore.WHITE+" Get Location "+Fore.GREEN+"[SMARTPHONES]\n")
+    print_and_sleep(Fore.RED+" [2]"+Fore.WHITE+" Access Webcam\n") 
+    print_and_sleep(Fore.RED+" [3]"+Fore.WHITE+" Access Microphone \n")
+    print_and_sleep(Fore.RED+" [4]"+Fore.WHITE+" Exit . . .\n")
